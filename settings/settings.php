f41b654cf3769651cf7840743ac53504bd270ed7
Luis DeLeon <luis.deleon@ebluenet.com>
2020-12-21 22:06:37 -0600

    Update settings.php

--- settings/settings.php
+++ settings/settings.php
@@ -180,7 +180,7 @@ if (!isset($_SESSION["mikhmon"])) {
 <div class="col-12">
 	<div class="card">
         <div class="card-header">
-            <h3 class="card-title">Mikhmon Data</h3>
+            <h3 class="card-title">CloudHotspot</h3>
         </div>
     <div class="card-body">    
 	<table class="table table-sm">
